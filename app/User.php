eb58708223a97bf691f67cf337a612f50641774d
juliajune <jljune3@gmail.com>
2018-07-30 11:14:03 +0300

    Create method for work with users in  Model User

--- app/User.php
+++ app/User.php
@@ -8,6 +8,12 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     use Notifiable;
+   
+    const IS_BANNED =1;
+    const IS_ACTIVE=0;
+   
+
+
 
     /**
      * The attributes that are mass assignable.
@@ -39,4 +45,123 @@ class User extends Authenticatable
    }
 
 
+// Methods. Work with DB
+public static function add($fields)
+{
+     $user=new static;  //create new object of User model
+     $user->fill($fields); // data from $fillable massive
+     $user->password=bcrypt($fields['password']);
+     $user->save();
+     return $user;
 }
+
+public static function edit($fields)
+{
+     $user->fill($fields); // data from $fillable massive
+     $user->password=bcrypt($fields['password']);
+     $user->save();
+}
+
+
+public static function remove()
+{
+     $this->delete();
+}
+
+
+//upload avatar
+      public function uploadAvatar($image)
+      {
+         //if $image is null do nothing
+         if($image==null) {return;}
+         
+         //If image already exists, delete it
+         Storage::delete('uploads/'. $this->image);
+         //Upload new image
+         $filename=str_random(10) . '.' . $image->extension();
+         $image->saveAS('uploads' , $filename); // 'uploads' - path to upload image
+         $this->image=$filename;
+         $this->save;
+      }
+
+
+      //Show post image
+     public function getImage()
+     {
+       //if image not exist, show default image
+       if ($this->image==null)
+       {
+        return '/img/no-user-image.png';
+       }
+         
+         //if image exist, show  image
+         return '/uploads/'.$this->image;
+     }
+
+     public function makeAdmin()
+     {
+       $this->is_admin=1;
+       $this->save();
+     }
+
+      public function makeNormal()
+     {
+       $this->is_admin=0;
+       $this->save();
+     }
+
+    public function toggleAdmin($value)
+    {
+       is ($value == null)
+       {
+        return $this->makeNormal();
+       }
+        
+        return $this->makeAdmin(); 
+    }
+
+   //User bun or unban
+
+   public function unban()
+    {
+       $this->status=User::IS_ACTIVE;
+       $this->save();
+    }
+
+   public function ban()
+    {
+       $this->status=User::IS_BANED;
+       $this->save();
+    }
+
+    public function toggleBan($value)
+    {
+      
+         if($value==null){
+             return $This->unban();
+
+          }
+
+          return $this->ban();
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}// User
